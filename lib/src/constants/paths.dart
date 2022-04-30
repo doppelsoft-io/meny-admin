1f8b1f0ff5d63b088e9b38814d1c844904e8d45c
Jack Grossmann <jackgro@icloud.com>
2022-04-30 15:02:31 -0700

    Update paths.dart

--- lib/src/constants/paths.dart
+++ lib/src/constants/paths.dart
@@ -4,7 +4,9 @@ class Paths {
   static const String stores = 'stores';
   static const String users = 'users';
   static const String menus = 'menus';
+  static const String menuCategories = 'menu_categories';
   static const String compiledMenus = 'compiledMenus';
   static const String menuItems = 'menu_items';
   static const String categories = 'categories';
+  static const String categoryMenuItems = 'category_menu_items';
 }
