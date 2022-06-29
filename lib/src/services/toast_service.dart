a1ae6500a3a960532d0398ac950d086702e68723
Jack Grossmann <jackgro@icloud.com>
2022-06-28 22:11:49 -0700

    Update toast_service.dart

--- lib/src/services/toast_service.dart
+++ lib/src/services/toast_service.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meny_admin/themes.dart';
 import 'package:overlay_support/overlay_support.dart' as lib;
 
 enum ToastType {
@@ -11,20 +12,68 @@ class ToastService {
 
   final ColorScheme colorScheme;
 
-  static void toast(String message) {
+  void toast(String message) {
     lib.toast(message);
+  }
+
+  void showOverlay(
+    Widget notification, [
+    ToastType? type = ToastType.info,
+  ]) {
+    lib.showOverlay(
+      (context, progress) {
+        return Transform.translate(
+          offset: Tween<Offset>(
+            begin: const Offset(0, 40),
+            end: Offset.zero,
+          ).transform(progress),
+          child: Opacity(
+            opacity: Tween<double>(
+              begin: 0,
+              end: 1,
+            ).transform(progress),
+            child: SafeArea(
+              child: DefaultTextStyle(
+                style: Themes.textStyle,
+                child: Center(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(4),
+                    child: Container(
+                      color: type == ToastType.error
+                          ? colorScheme.error
+                          : colorScheme.primary,
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 12,
+                        horizontal: 48,
+                      ),
+                      child: notification,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
   }
 
   void showNotification(
     Widget notification, [
     ToastType? type = ToastType.info,
   ]) {
+    final backgroundColor =
+        type == ToastType.error ? colorScheme.error : colorScheme.primary;
+
+    final foregroundColor =
+        type == ToastType.error ? colorScheme.onError : colorScheme.onPrimary;
+
     lib.showSimpleNotification(
       notification,
-      background:
-          type == ToastType.error ? colorScheme.error : colorScheme.primary,
-      foreground:
-          type == ToastType.error ? colorScheme.onError : colorScheme.onPrimary,
+      background: backgroundColor,
+      foreground: foregroundColor,
+
+      // position: lib.NotificationPosition.bottom,
     );
   }
 }
