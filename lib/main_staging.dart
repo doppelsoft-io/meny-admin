b1061a98c6be53ab76551e6f4325a8c23414d276
Jack Grossmann <jackgro@icloud.com>
2022-06-30 19:34:44 -0700

    Staging configs

--- lib/main_staging.dart
+++ lib/main_staging.dart
@@ -2,12 +2,15 @@ import 'package:doppelsoft_core/doppelsoft_core.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:meny_admin/app.dart';
+import 'package:meny_admin/firebase_options_stg.dart';
 import 'package:meny_admin/locator.dart';
 import 'package:meny_admin/src/services/services.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   await Locator.setup();
   await HiveService.configure();
