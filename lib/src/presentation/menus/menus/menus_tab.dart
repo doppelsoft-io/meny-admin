83c763b89102cd0dcbec142e968674e3702f48b1
Jack Grossmann <jackgro@icloud.com>
2022-07-06 20:01:17 -0700

    Update menus_tab.dart

--- lib/src/presentation/menus/menus/menus_tab.dart
+++ lib/src/presentation/menus/menus/menus_tab.dart
@@ -7,29 +7,39 @@ import 'package:meny_admin/src/constants/analytics.dart';
 import 'package:meny_admin/src/data/menus/menus.dart';
 import 'package:meny_admin/src/data/stores/stores.dart';
 import 'package:meny_admin/src/presentation/menus/menus.dart';
+import 'package:meny_admin/src/presentation/menus/menus/widgets/new_menu_button.dart';
 import 'package:meny_admin/src/presentation/resources/cubit/resources_cubit.dart';
-import 'package:meny_admin/src/presentation/shared/shared.dart';
 import 'package:meny_admin/src/services/services.dart';
 import 'package:meny_core/meny_core.dart';
 
-class MenusMenusTab extends StatelessWidget {
-  const MenusMenusTab({Key? key}) : super(key: key);
+class MenusScreenMenusTab extends StatelessWidget {
+  const MenusScreenMenusTab({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<ResourcesCubit>(
-      create: (context) => ResourcesCubit<MenuModel>(
-        iResourcesRepository: Locator.instance<MenuRepository>(),
-      ),
-      child: _MenusMenusTab(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<ResourcesCubit>(
+          create: (context) => ResourcesCubit<MenuModel>(
+            iResourcesRepository: Locator.instance<MenuRepository>(),
+          ),
+        ),
+        BlocProvider<CreateMenuCubit>(
+          create: (context) => CreateMenuCubit(
+            storeCubit: context.read<StoreCubit>(),
+          ),
+        ),
+      ],
+      child: _MenusScreenMenusTab(),
     );
   }
 }
 
-class _MenusMenusTab extends HookWidget {
+class _MenusScreenMenusTab extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final menusState = context.watch<ResourcesCubit>().state;
+    final createMenuState = context.watch<CreateMenuCubit>().state;
     final menus = List<MenuModel>.from(menusState.resources);
 
     useEffect(
@@ -51,105 +61,107 @@ class _MenusMenusTab extends HookWidget {
         }
       },
       child: SingleChildScrollView(
-        child: DTable(
-          args: DTableArgs(
-            header: DText.headline5('Menus'),
-            actions: [
-              PageActionButton(
-                title: 'New',
-                onPressed: () {
-                  ActionService.run(
-                    () => UpdateMenusSheet.open(
-                      context: context,
-                      menu: MenuModel.empty(),
-                    ),
-                    () => AnalyticsService.track(
-                      message: Analytics.menusTabNewTapped,
-                    ),
-                  );
-                },
-              ),
-            ],
-            columns: [
-              const DTableHeader(name: 'Name'),
-              const DTableHeader(name: 'Last Updated'),
-              const DTableHeader(name: 'Actions'),
-            ],
-            source: DTableDataSource(
-              emptyBuilder: (context) {
-                return Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Divider(height: kMinInteractiveDimension),
-                    DText.bodyText1(
-                      'No menus yet. Click "New" above to add a menu',
-                    ),
-                    const Divider(height: kMinInteractiveDimension),
-                  ],
-                );
-              },
-              rows: menus
-                  .map(
-                    (e) => DTableRow(
-                      onSelectChanged: (selected) {
-                        ActionService.run(
-                          () => UpdateMenusSheet.open(
-                            context: context,
-                            menu: e,
-                          ),
-                          () => AnalyticsService.track(
-                            message: Analytics.menusTabItemSelected,
-                          ),
+        child: createMenuState.maybeWhen(
+          creating: (_) {
+            return const CircularProgressIndicator();
+          },
+          orElse: () {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                DTable(
+                  args: DTableArgs(
+                    header: DText.headline5('Menus'),
+                    actions: [
+                      const NewMenuButton(),
+                    ],
+                    columns: [
+                      const DTableHeader(name: 'Name'),
+                      const DTableHeader(name: 'Last Updated'),
+                      const DTableHeader(name: 'Actions'),
+                    ],
+                    source: DTableDataSource(
+                      emptyBuilder: (context) {
+                        return Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Divider(height: kMinInteractiveDimension),
+                            DText.bodyText1(
+                              'No menus yet. Click "New" above to add a menu',
+                            ),
+                            const Divider(height: kMinInteractiveDimension),
+                          ],
                         );
                       },
-                      cells: [
-                        DTableCell(
-                          builder: () => DText.bodyText1(e.name),
-                        ),
-                        DTableCell(
-                          builder: () {
-                            return DText.bodyText1(
-                              e.updatedAt?.formatWith('MM/dd/yy @ h:mm a') ??
-                                  '',
-                            );
-                          },
-                        ),
-                        DTableCell(
-                          builder: () {
-                            return Row(
-                              children: [
-                                TextButton(
-                                  style: ButtonStyle(
-                                    alignment: Alignment.center,
-                                    padding: MaterialStateProperty.all(
-                                      EdgeInsets.zero,
-                                    ),
+                      rows: menus
+                          .map(
+                            (e) => DTableRow(
+                              onSelectChanged: (selected) {
+                                ActionService.run(
+                                  () {},
+                                  () => AnalyticsService.track(
+                                    message: Analytics.menusTabItemSelected,
                                   ),
-                                  child: const Text('Preview'),
-                                  onPressed: () => ActionService.run(
-                                    () => Navigator.of(context).pushNamed(
-                                      MenuPreviewScreen.routeName,
-                                      arguments: MenuPreviewScreenArgs(menu: e),
-                                    ),
-                                    () => AnalyticsService.track(
-                                      message: Analytics.menusTabPreviewTapped,
-                                      params: {
-                                        'menuId': e.id!,
-                                        'menuName': e.name,
-                                      },
-                                    ),
-                                  ),
+                                );
+                              },
+                              cells: [
+                                DTableCell(
+                                  builder: () => DText.bodyText1(e.name),
+                                ),
+                                DTableCell(
+                                  builder: () {
+                                    return DText.bodyText1(
+                                      e.updatedAt?.formatWith(
+                                            'MM/dd/yy @ h:mm a',
+                                          ) ??
+                                          '',
+                                    );
+                                  },
+                                ),
+                                DTableCell(
+                                  builder: () {
+                                    return Row(
+                                      children: [
+                                        TextButton(
+                                          style: ButtonStyle(
+                                            alignment: Alignment.center,
+                                            padding: MaterialStateProperty.all(
+                                              EdgeInsets.zero,
+                                            ),
+                                          ),
+                                          child: const Text('Preview'),
+                                          onPressed: () => ActionService.run(
+                                            () =>
+                                                Navigator.of(context).pushNamed(
+                                              MenuPreviewScreen.routeName,
+                                              arguments: MenuPreviewScreenArgs(
+                                                menu: e,
+                                              ),
+                                            ),
+                                            () => AnalyticsService.track(
+                                              message: Analytics
+                                                  .menusTabPreviewTapped,
+                                              params: {
+                                                'menuId': e.id!,
+                                                'menuName': e.name,
+                                              },
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    );
+                                  },
                                 ),
                               ],
-                            );
-                          },
-                        ),
-                      ],
+                            ),
+                          )
+                          .toList(),
                     ),
-                  )
-                  .toList(),
-            ),
-          ),
+                  ),
+                ),
+              ],
+            );
+          },
         ),
       ),
     );
