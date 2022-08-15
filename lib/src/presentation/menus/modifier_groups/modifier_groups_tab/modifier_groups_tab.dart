3a9399237e2134cf4a93ec65e8319e9e02cf3dd0
Jack Grossmann <jackgro@icloud.com>
2022-08-14 22:09:02 -0700

    emptyMessage for modifier groups

--- lib/src/presentation/menus/modifier_groups/modifier_groups_tab/modifier_groups_tab.dart
+++ lib/src/presentation/menus/modifier_groups/modifier_groups_tab/modifier_groups_tab.dart
@@ -159,7 +159,8 @@ class _MenusScreenModifierGroupsTab extends HookWidget {
                     ),
                   );
                 },
-                emptyMessage: '',
+                emptyMessage:
+                    'No modifier groups yet. Click "New" above to get started!',
               ),
             ),
           ],
