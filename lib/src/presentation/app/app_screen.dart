2203e05976036bffa10a760c72019d0122e1ea5f
Jack Grossmann <jackgro@icloud.com>
2022-08-15 17:40:25 -0700

    Update navigation rail

--- lib/src/presentation/app/app_screen.dart
+++ lib/src/presentation/app/app_screen.dart
@@ -1,5 +1,4 @@
 import 'package:doppelsoft_core/doppelsoft_core.dart';
-import 'package:doppelsoft_ui/doppelsoft_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -166,27 +165,24 @@ class _DesktopLayout extends StatelessWidget {
       ),
       body: Row(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(DSSpacing.medium),
-            child: NavigationRail(
-              extended: getValueForScreenType<bool>(
-                context: context,
-                mobile: false,
-                tablet: false,
-                desktop: true,
-              ),
-              selectedIndex: selectedIndex,
-              onDestinationSelected: onSelect,
-              destinations: tabs
-                  .map(
-                    (tuple) => NavigationRailDestination(
-                      icon: Icon(tuple.value1),
-                      selectedIcon: Icon(tuple.value2),
-                      label: Text(tuple.value3),
-                    ),
-                  )
-                  .toList(),
+          NavigationRail(
+            extended: getValueForScreenType<bool>(
+              context: context,
+              mobile: false,
+              tablet: false,
+              desktop: true,
             ),
+            selectedIndex: selectedIndex,
+            onDestinationSelected: onSelect,
+            destinations: tabs
+                .map(
+                  (tuple) => NavigationRailDestination(
+                    icon: Icon(tuple.value1),
+                    selectedIcon: Icon(tuple.value2),
+                    label: Text(tuple.value3),
+                  ),
+                )
+                .toList(),
           ),
           VerticalDivider(
             width: 3,
